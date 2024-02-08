ed7b60a0bc75067ae848ea52d90985a6dbe7d1f8
Eder Daniel Domingues <eder.infonet@gmail.com>
2024-02-08 07:32:32 -0400

    feat: Im plementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,19 @@
 from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 
 
 def handler_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        # Aqui se pode colocar outros processos e validação
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
     return HttpResponse(
         status_code=500,
         body={
